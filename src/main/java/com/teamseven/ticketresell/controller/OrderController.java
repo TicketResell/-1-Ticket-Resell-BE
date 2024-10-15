bdbab43beca5bc60524092db6e10c3d88710b636
Micu <cuongtranminh510@gmail.com>
2024-10-15 23:50:56 +0700

    View order api and cate image

--- src/main/java/com/teamseven/ticketresell/controller/OrderController.java
+++ src/main/java/com/teamseven/ticketresell/controller/OrderController.java
@@ -2,10 +2,18 @@ package com.teamseven.ticketresell.controller;
 
 import com.teamseven.ticketresell.converter.OrderConverter;
 import com.teamseven.ticketresell.dto.OrderDTO;
+import com.teamseven.ticketresell.entity.OrderEntity;
+import com.teamseven.ticketresell.entity.TicketEntity;
+import com.teamseven.ticketresell.repository.OrderRepository;
 import com.teamseven.ticketresell.service.impl.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -16,6 +24,9 @@ public class OrderController {
 
     @Autowired
     private OrderConverter orderConverter;
+
+    @Autowired
+    private OrderRepository orderRepository;
     // API tạo đơn hàng mới
     @PostMapping("/create")
     public ResponseEntity<?> createOrder(@RequestBody OrderDTO orderDTO) {
@@ -27,6 +38,34 @@ public class OrderController {
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Error occurred: " + e.getMessage());
         }
+    }
+    @GetMapping
+    public ResponseEntity<?> getAllOrders() {
+        try {
+        List<OrderEntity> orders = orderRepository.findAll();
+        if (orders != null) {
+            return ResponseEntity.ok(orders.stream().map(orderConverter::toDTO).toList());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Have not any order yet!");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error occurred: " + e.getMessage());
+        }
+    }
+    @GetMapping("/seller/{sellerId}")
+    public ResponseEntity<?> getOrdersBySeller(@PathVariable Long sellerId) {
+        List<OrderDTO> orders = orderService.getOrdersBySeller(sellerId);
+        if (orders != null) {
+            return ResponseEntity.ok(orders);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Have not any sell order yet!");
+    }
+    @GetMapping("/buyer/{buyerId}")
+    public ResponseEntity<?> getOrdersByBuyer(@PathVariable Long buyerId) {
+        List<OrderDTO> orders = orderService.getOrdersByBuyer(buyerId);
+        if (orders != null) {
+            return ResponseEntity.ok(orders);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Have not any buy order yet!");
     }
 }
 
--- src/main/java/com/teamseven/ticketresell/repository/OrderRepository.java
+++ src/main/java/com/teamseven/ticketresell/repository/OrderRepository.java
@@ -4,7 +4,12 @@ import com.teamseven.ticketresell.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
     // Bạn có thể thêm các phương thức tìm kiếm tùy chỉnh ở đây nếu cần
+    List<OrderEntity> findBySeller_Id(Long sellerId);  // Tìm đơn hàng theo sellerId
+    List<OrderEntity> findByBuyer_Id(Long buyerId);  // Tìm đơn hàng theo buyerId
+
 }
