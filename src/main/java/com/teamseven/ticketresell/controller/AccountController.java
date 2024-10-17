b36d2946b6de9d3e8aa940bf71601f952659ea4e
minhtris-peternguyxn <minhtri10504@gmail.com>
2024-10-17 14:19:16 +0700

    Update AccountController.java
    commit again because cause of conflit

--- src/main/java/com/teamseven/ticketresell/controller/AccountController.java
+++ src/main/java/com/teamseven/ticketresell/controller/AccountController.java
@@ -29,7 +29,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
-    private String googleClientId = "310764216947-glor2ci0tha7scaf77cgmiqrod6c58fq.apps.googleusercontent.com";
+
 
     @Autowired
     private UserService userService;
@@ -51,149 +51,67 @@ public class AccountController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Map<String, String> loginRequest) {
-        // Lấy giá trị từ request body
         String identifier = loginRequest.get("identifier");
-        String password = loginRequest.get( "password");
+        String password = loginRequest.get("password");
 
-        // In thông tin ra để kiểm tra
-        System.out.println("Identifier: " + identifier);
-        System.out.println("Password: " + password);
-
-        // Xác thực người dùng dựa trên email hoặc username và mật khẩu
-        UserDTO result = userService.login(identifier, password);
-        String exceptionMessage = "";
-
-        // Kiểm tra trạng thái người dùng
-        if (result != null) {
-            System.out.println("User status: " + result.getStatus());
-            System.out.println("Is Verified: " + result.isVerifiedEmail());
-
-            if (result.getStatus().equals("banned")) {
-                exceptionMessage = "Your account has been banned.";
-            } else if (!result.isVerifiedEmail()) {
-                exceptionMessage = "You are not verified.";
-            }
-
-            if (!exceptionMessage.isBlank()) {
-                System.out.println(exceptionMessage);
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exceptionMessage);
-            }
+        try {
+            UserDTO userDTO = userService.login(identifier, password);
 
             // Tạo JWT nếu thông tin hợp lệ
-            String jwt = jwtUtil.generateToken(result.getId(),result.getUsername(), result.getEmail(), result.getFullname(), result.getUserImage(), result.getRole());
-            System.out.println("DTO: " + result);
+            String jwt = jwtUtil.generateToken(userDTO.getId(), userDTO.getUsername(), userDTO.getEmail(), userDTO.getFullname(), userDTO.getUserImage(), userDTO.getRole());
             return ResponseEntity.ok(new JwtResponse(jwt));
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
         }
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Email hoặc mật khẩu không hợp lệ");
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserDTO userDTO) {
-        if (userRepository.findByUsername(userDTO.getUsername()) != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists");
+        try {
+            UserDTO registeredUser = userService.register(userDTO);
+            return ResponseEntity.ok("Registration successful. Please check your email to verify your account.");
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
         }
-
-        // Kiểm tra email đã tồn tại chưa
-        if (userRepository.findByEmail(userDTO.getEmail()) != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email already exists");
-        }
-
-        System.out.println("REQUEST DTO:" + userDTO);
-
-        // Chuyển đổi DTO thành Entity và lưu user
-        UserEntity newUser = accountConverter.toEntity(userDTO);
-        newUser.setCreatedDate(LocalDateTime.now());
-        newUser.setRole("user"); //default == user.
-        newUser.setStatus("active");
-
-
-        newUser.setCreatedDate(LocalDateTime.now());
-        userDTO.setVerifiedEmail(false);
-        UserEntity savedUser = userRepository.save(newUser);
-        System.out.println("SAVED USER:"+savedUser);
-
-
-        // Gửi email xác thực
-        String verificationLink = "http://localhost:8084/api/accounts/verify?email=" + userDTO.getEmail();
-        String subject = "Email Verification";
-        String body = "Thank you for registering. Please click the link below to verify your email:\n" + verificationLink;
-
-        emailService.sendEmail(userDTO.getEmail(), subject, body);
-
-        return ResponseEntity.ok("Registration successful. Please check your email to verify your account.");
     }
+
     // create account
     @PostMapping("/staff")
     public ResponseEntity<?> createAccount(@RequestBody UserDTO userDTO) {
-        if (userRepository.findByUsername(userDTO.getUsername()) != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists");
+        try {
+            UserEntity savedUser = userService.createNewAccount(userDTO.getUsername(),userDTO.getPassword());
+            return ResponseEntity.ok("Create account successful");
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
         }
-
-        // Kiểm tra email đã tồn tại chưa
-        if (userRepository.findByEmail(userDTO.getEmail()) != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email already exists");
-        }
-
-        System.out.println("REQUEST DTO:" + userDTO);
-
-        // Chuyển đổi DTO thành Entity và lưu user
-        UserEntity newUser = accountConverter.toEntity(userDTO);
-        newUser.setCreatedDate(LocalDateTime.now());
-//        newUser.setRole("user"); //default == user.
-        newUser.setStatus("active");
-
-        newUser.setCreatedDate(LocalDateTime.now());
-        userDTO.setVerifiedEmail(true);
-        UserEntity savedUser = userRepository.save(newUser);
-        System.out.println("SAVED USER:"+savedUser);
-
-        return ResponseEntity.ok("Create account successful");
     }
+
     // get all accounts
     @GetMapping("/staff")
     public ResponseEntity<?> getAccounts() {
         List<UserEntity> accounts = userRepository.findAll();
         return ResponseEntity.ok(accounts.stream().map(accountConverter::toDTO).toList());
     }
-    // update account
+
     @PutMapping("/staff/{id}")
     public ResponseEntity<?> updateUser(@PathVariable("id") Long id, @RequestBody UserDTO userDTO) {
-        // Kiểm tra xem user có tồn tại không
-        UserEntity existingUser = userService.findById(id);
-        if (existingUser == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        try {
+            UserEntity updatedUser = userService.updateUser(id, userDTO);
+            return ResponseEntity.ok("User updated successfully");
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
         }
-
-        // Cập nhật thông tin người dùng
-        existingUser.setUsername(userDTO.getUsername());
-        existingUser.setEmail(userDTO.getEmail());
-        existingUser.setPhone(userDTO.getPhone());
-        existingUser.setAddress(userDTO.getAddress());
-        existingUser.setFullname(userDTO.getFullname());
-        existingUser.setUserImage(userDTO.getUserImage());
-        existingUser.setStatus(userDTO.getStatus());
-        existingUser.setVerifiedEmail(userDTO.isVerifiedEmail());
-        existingUser.setRole(userDTO.getRole());
-
-        // Lưu lại user đã cập nhật
-        UserEntity updatedUser = userService.save(existingUser);
-
-        return ResponseEntity.ok("User updated successfully");
     }
-    // delete account by id
+
     @DeleteMapping("/staff/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable("id") Long id) {
-        // Kiểm tra xem user có tồn tại không
-        UserEntity existingUser = userService.findById(id);
-        if (existingUser == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        try {
+            userService.delete(id);
+            return ResponseEntity.ok("User deleted successfully");
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
         }
-
-        // Xóa tài khoản người dùng
-        userService.delete(id);
-
-        return ResponseEntity.ok("User deleted successfully");
     }
     // reset password by mail
     @PostMapping("/reset")
@@ -206,7 +124,7 @@ public class AccountController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email was not registered before!");
         }
 
-        // Xử lý logic gửi email xác thực
+        // gửi email xác thực
         String verificationLink = "http://localhost:8084/api/accounts/reset?email=" + email;
         String subject = "Reset Password";
         String body = "Please click the link below to reset your password:\n" + verificationLink;
@@ -215,94 +133,71 @@ public class AccountController {
 
         return ResponseEntity.ok("Reset password link has been sent to your email!");
     }
+
     // update new password
-    @PostMapping("/update-password")
+    @PutMapping("/update-password")
     public ResponseEntity<?> updatePassword(@RequestBody Map<String, String> request) {
-        String email = request.get("email");
-        String newPassword = request.get("newPassword");
+        try {
+            String email = request.get("email");
+            String newPassword = request.get("newPassword");
 
-        // Kiểm tra email có tồn tại trong database không
-        UserEntity user = userRepository.findByEmail(email);
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid email!");
+            userService.updatePassword(email, newPassword);
+            return ResponseEntity.ok("Password updated successfully!");
+        } catch (RuntimeException ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
         }
-
-        // Cập nhật mật khẩu mới=
-        user.setPassword(newPassword);
-        userRepository.save(user);  // Lưu user với mật khẩu mới
-
-        return ResponseEntity.ok("Password has been updated successfully!");
     }
 
 
+    // verify
     @GetMapping("/verify")
     public ResponseEntity<?> verifyEmail(@RequestParam("email") String email) {
-        UserEntity user = userRepository.findByEmail(email);
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email not found");
+        try {
+            userService.verifyEmail(email);
+            return ResponseEntity.ok("Email verified successfully.");
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
         }
-
-        // Cập nhật trạng thái xác thực email
-        user.setVerifiedEmail(true);
-        userRepository.save(user);
-
-        return ResponseEntity.ok("Email verified successfully.");
     }
-
 
     @GetMapping("/profile/{username}")
     public ResponseEntity<?> viewProfile(@PathVariable String username) {
-        // Lấy đối tượng Authentication từ SecurityContext
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        try {
+            // Lấy đối tượng Authentication từ SecurityContext
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        // Kiểm tra xác thực
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+            // Kiểm tra xác thực
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+            }
+
+            // Lấy tên người dùng hiện tại từ Authentication
+            String currentUser = authentication.getName();
+
+            UserDTO userDTO = userService.viewProfile(username, currentUser);
+            return ResponseEntity.ok(userDTO);
+
+        } catch (RuntimeException ex) {
+            if (ex.getMessage().equals("User not found")) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+            } else { // "You are not allowed to view this profile"
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+            }
         }
-
-        // Lấy tên người dùng hiện tại từ Authentication
-        String currentUser = authentication.getName();
-
-        // Tìm người dùng theo username
-        UserEntity user = userRepository.findByUsername(username);
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
-        }
-
-        // Lấy vai trò từ username
-        String role = userService.getUserRoleByUsername(user.getUsername());
-
-        // Kiểm tra quyền truy cập dựa trên vai trò và tên người dùng hiện tại
-        if (role.equals("ADMIN") || role.equals("STAFF") || currentUser.equals(user.getUsername())) {
-            // Trả về thông tin người dùng nếu đủ quyền
-            return ResponseEntity.ok(accountConverter.toDTO(user));
-        }
-
-        // Nếu không đủ quyền, trả về lỗi 403
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You are not allowed to view this profile");
     }
-
 
 
     // Modify Profile
     @PutMapping("/profile/{username}")
-    public ResponseEntity<?> editProfile(@PathVariable String username, @RequestBody UserDTO userDTO, @RequestParam boolean isAdmin) {
-        // Find username
-        UserEntity existingUser = userRepository.findByUsername(username);
-
-        if (existingUser != null) {
-            // Convert DTO -> Entity
-            UserEntity updatedUser = accountConverter.toEntity(userDTO, existingUser, isAdmin);
-
-            // Save to db
-            userRepository.save(updatedUser);
-
-            // ìf everything runs smoothly, return DTO
-            return ResponseEntity.ok(accountConverter.toDTO(updatedUser));
+    public ResponseEntity<?> editProfile(@PathVariable String username,
+                                         @RequestBody UserDTO userDTO,
+                                         @RequestParam boolean isAdmin) {
+        try {
+            UserDTO updatedUserDTO = userService.editProfile(username, userDTO, isAdmin);
+            return ResponseEntity.ok(updatedUserDTO);
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
         }
-
-        // if user not exist, return 404
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
     }
 
 
@@ -310,43 +205,11 @@ public class AccountController {
     @PostMapping("/login-google")
     public ResponseEntity<?> loginWithGoogle(@RequestBody Map<String, String> body) {
         String idTokenString = body.get("id_token");
-        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-                .setAudience(Collections.singletonList(googleClientId))
-                .build();
-
         try {
-            GoogleIdToken idToken = verifier.verify(idTokenString);
-            if (idToken != null) {
-                GoogleIdToken.Payload payload = idToken.getPayload();
-                String email = payload.getEmail();
-                String name = (String) payload.get("name");
-
-
-                // Check if user exists
-                UserEntity user = userRepository.findByEmail(email);
-                if (user == null) {
-                    // Create a new user if not exists
-                    user = new UserEntity();
-                    user.setUsername(email + "_google"); // Đảm bảo username không bị trùng
-                    user.setPassword(email + "_password"); // Đặt tạm mật khẩu
-                    user.setEmail(email);
-                    user.setStatus("active");
-                    user.setRole("user"); //default == user.
-                    user.setCreatedDate(LocalDateTime.now());
-                    user.setVerifiedEmail(true);
-                    user = userRepository.save(user);
-                }
-
-                // Generate JWT token
-                String jwt = jwtUtil.generateToken(user.getId(),user.getUsername(),user.getEmail(), user.getFullname(), user.getUserImage(), user.getRole());
-
-                // Return user info and JWT
-                return ResponseEntity.ok(new JwtResponse(jwt));
-            } else {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Google token");
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + e.getMessage());
+            JwtResponse jwtResponse = userService.loginWithGoogle(idTokenString);
+            return ResponseEntity.ok(jwtResponse);
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
         }
     }
 
