7774fe654ad83533a928a8cd1e4fd2db67bab3d4
Micutran5 <cuongtranminh510@gmail.com>
2024-10-23 13:11:15 +0700

    fix balance trans order

--- src/main/java/com/teamseven/ticketresell/service/impl/OrderService.java
+++ src/main/java/com/teamseven/ticketresell/service/impl/OrderService.java
@@ -131,13 +131,19 @@ public class OrderService {
         OrderEntity order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new IllegalArgumentException("Order not found!"));
         // Kiểm tra nếu trạng thái đơn hàng được cập nhật thành "complete"
-        if ("Complete".equalsIgnoreCase(orderStatus)) {
+        if ("completed".equalsIgnoreCase(orderStatus)) {
             if (OrderEntity.PaymentStatus.paid.equals(order.getPaymentStatus())){
                 order.setPaymentStatus(OrderEntity.PaymentStatus.paid);
                 transactionService.createBuyerTransaction(order);
             }
             order.setOrderStatus(OrderEntity.OrderStatus.completed);
             transactionService.createSellerTransaction(order);
+        } else if ("shipping".equalsIgnoreCase(orderStatus)) {
+            // Xử lý khi trạng thái là "shipping"
+            order.setOrderStatus(OrderEntity.OrderStatus.shipping);
+        } else if ("received".equalsIgnoreCase(orderStatus)) {
+            // Xử lý khi trạng thái là "received"
+            order.setOrderStatus(OrderEntity.OrderStatus.received);
         } else {
             throw new IllegalArgumentException("Invalid order status");
         }
