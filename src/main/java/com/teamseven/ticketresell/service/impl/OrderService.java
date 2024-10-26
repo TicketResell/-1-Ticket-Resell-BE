e5e2a3545f1ac58ae9347c509a96503da985f850
Micutran5 <cuongtranminh510@gmail.com>
2024-10-26 08:39:52 +0700

    fix logic agency

--- src/main/java/com/teamseven/ticketresell/service/impl/OrderService.java
+++ src/main/java/com/teamseven/ticketresell/service/impl/OrderService.java
@@ -68,7 +68,11 @@ public class OrderService {
         OrderEntity orderEntity = orderConverter.toEntity(orderDTO);
         // Lưu vào cơ sở dữ liệu
         orderEntity.setCreatedDate(LocalDateTime.now());
-        orderEntity.setServiceFee(0.05);
+        if (seladdler.isAgency()) {
+            orderEntity.setServiceFee(0.05); // Nếu là agency, set phí dịch vụ thành 0.03
+        } else {
+            orderEntity.setServiceFee(0.1); // Nếu không phải agency, set phí dịch vụ thành 0.05
+        }
         OrderEntity savedOrder = orderRepository.save(orderEntity);
         // Trả về OrderDTO sau khi lưu
         return orderConverter.toDTO(savedOrder);
