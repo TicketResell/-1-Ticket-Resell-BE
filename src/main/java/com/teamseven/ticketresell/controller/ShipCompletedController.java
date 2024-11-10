d2b4cbada748f9586a647c2377a6829f83a14e5d
Micutran5 <cuongtranminh510@gmail.com>
2024-11-10 10:22:56 +0700

    fix receive or bomb status

--- src/main/java/com/teamseven/ticketresell/controller/ShipCompletedController.java
+++ src/main/java/com/teamseven/ticketresell/controller/ShipCompletedController.java
@@ -41,69 +41,8 @@ public class ShipCompletedController {
 
     @Autowired
     private TransactionService transactionService;
-
-    @PutMapping("/set-shipping-status-false/{orderId}")
-    public ResponseEntity<?> updateOrderStatusFalse(@PathVariable Long orderId, String img) {
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: No authentication provided.");
-        }
-
-        String username = authentication.getName();
-        String userRole = userService.getUserRoleByUsername(username);
-
-        if (!userRole.equals("shipper")) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: Role - " + userRole);
-        }
-
-        OrderEntity orderEntity = orderRepository.findById(orderId).orElse(null);
-
-        if (orderEntity == null) {
-            return ResponseEntity.badRequest().body("NOT FOUND FROM DB 404");
-        }
-        orderEntity.setImgShiper(img);
-        orderEntity.setOrderStatus(OrderEntity.OrderStatus.orderbombing);
-        if (OrderEntity.PaymentStatus.paid.equals(orderEntity.getPaymentStatus())){
-            transactionService.createSellerTransaction(orderEntity);
-        }
-        orderRepository.save(orderEntity);
-        orderRepository.flush();
-
-        return ResponseEntity.ok().build();
-    }
-
-
-    @GetMapping("/show-all-order/ship")
-    public ResponseEntity<?> showAllOrderForShip() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: No authentication provided.");
-        }
-
-        String username = authentication.getName();
-        String userRole = userService.getUserRoleByUsername(username);
-
-        if (!userRole.equals("shipper")) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: Role - " + userRole);
-        }
-
-        // Lấy tất cả các đơn hàng có trạng thái và phương thức thanh toán mong muốn
-        List<OrderEntity> orders = orderRepository.findByOrderStatus(
-                OrderEntity.OrderStatus.shipping
-        );
-        List<OrderDTO> dtos = new ArrayList<>();
-        for (OrderEntity order : orders) {
-            OrderDTO dto = orderConverter.toDTO(order);
-            dtos.add(dto);
-        }
-
-        return ResponseEntity.ok(dtos);
-    }
     @PutMapping("/set-shipping-status/{orderId}")
-    public ResponseEntity<?> updateOrderStatusTrue(@PathVariable Long orderId, String img) {
+    public ResponseEntity<?> updateOrderStatusTrue(@PathVariable Long orderId,  @RequestBody Map<String, String> requestBody) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -117,8 +56,13 @@ public class ShipCompletedController {
         if (!userRole.equals("shipper")) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: Role - " + userRole);
         }
-        OrderEntity orderEntity = orderRepository.findById(orderId).orElse(null);
 
+        String img = requestBody.get("img");
+        if (img == null || img.isEmpty()) {
+            return ResponseEntity.badRequest().body("Image URL is missing.");
+        }
+
+        OrderEntity orderEntity = orderRepository.findById(orderId).orElse(null);
         if (orderEntity == null) {
             return ResponseEntity.badRequest().body("NOT FOUND FROM DB 404");
         }
@@ -131,7 +75,40 @@ public class ShipCompletedController {
         orderEntity.setRefundDeadline(LocalDateTime.now().plusDays(7));
         orderRepository.save(orderEntity);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(orderEntity);
+    }
+    @PutMapping("/set-shipping-status-false/{orderId}")
+    public ResponseEntity<?> updateOrderStatusFalse(@PathVariable Long orderId, @RequestBody Map<String, String> requestBody) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: No authentication provided.");
+        }
+
+        String username = authentication.getName();
+        String userRole = userService.getUserRoleByUsername(username);
+
+        if (!userRole.equals("shipper")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: Role - " + userRole);
+        }
+        String img = requestBody.get("img");
+        if (img == null || img.isEmpty()) {
+            return ResponseEntity.badRequest().body("Image URL is missing.");
+        }
+        OrderEntity orderEntity = orderRepository.findById(orderId).orElse(null);
+        if (orderEntity == null) {
+            return ResponseEntity.badRequest().body("NOT FOUND FROM DB 404");
+        }
+        orderEntity.setImgShiper(img);
+        orderEntity.setOrderStatus(OrderEntity.OrderStatus.orderbombing);
+        if (OrderEntity.PaymentStatus.paid.equals(orderEntity.getPaymentStatus())){
+            transactionService.createSellerTransaction(orderEntity);
+        }
+        orderRepository.save(orderEntity);
+        orderRepository.flush();
+
+        return ResponseEntity.ok(orderEntity);
     }
     @GetMapping("/all-order/ship")
     public ResponseEntity<?> allOrderForShipper() {
